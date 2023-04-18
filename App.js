ab7c7416bb8398ee822538b458376403b1088f8b
rayGreycloud <raygreycloud@gmail.com>
2023-04-18 12:41:10 -0700

    Add context provider; logout btn

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
@@ -5,7 +6,10 @@ import { StatusBar } from 'expo-status-bar';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import IconButton from './components/UI/IconButton';
 import { Colors } from './constants/styles';
+
+import AuthContextProvider, { AuthContext } from './store/auth-context';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +29,8 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -33,15 +39,31 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 }
       }}
     >
-      <Stack.Screen name='Welcome' component={WelcomeScreen} />
+      <Stack.Screen
+        name='Welcome'
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon='exit'
+              color={tintColor}
+              size={24}
+              onPress={() => authCtx.logout()}
+            />
+          )
+        }}
+      />
     </Stack.Navigator>
   );
 }
 
 function Navigation() {
+  const authCtx = useContext(AuthContext);
+  const isAuthenticated = authCtx.isAuthenticated;
+
   return (
     <NavigationContainer>
-      <AuthStack />
+      {!isAuthenticated ? <AuthStack /> : <AuthenticatedStack />}
     </NavigationContainer>
   );
 }
@@ -50,8 +72,9 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-
-      <Navigation />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
